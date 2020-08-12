2856d84018ccc3f689221e91c2bf6d7b17e785d8
Williano <paawilly17@gmail.com>
2020-08-11 22:38:05 -0500

    Add tween animation

--- flutter-animations/ninja_trips/lib/shared/screenTitle.dart
+++ flutter-animations/ninja_trips/lib/shared/screenTitle.dart
@@ -7,9 +7,23 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      duration: Duration(seconds: 5),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (context, double _value, Widget child) {
+        // return Opacity(opacity: _value, child: child);
+        return Opacity(
+          opacity: _value,
+          child: Padding(
+            padding: EdgeInsets.only(top: _value * 20),
+            child: child,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
