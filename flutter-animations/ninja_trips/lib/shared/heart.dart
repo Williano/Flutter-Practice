e4ea53a3cf64a71c71ca8c2566bc02f0b3202fbf
Williano <paawilly17@gmail.com>
2020-08-13 00:08:57 -0500

    Add explicit animation

--- flutter-animations/ninja_trips/lib/shared/heart.dart
+++ flutter-animations/ninja_trips/lib/shared/heart.dart
@@ -5,16 +5,59 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _colorAnimation;
+  bool _isFav = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(microseconds: 500),
+    )
+      ..addListener(() {})
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            _isFav = true;
+          });
+        }
+
+        if (status == AnimationStatus.dismissed) {
+          setState(() {
+            _isFav = false;
+          });
+        }
+      });
+
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, Widget child) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            _isFav ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
