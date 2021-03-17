5d63a554be429644027e5cd0eb6ea8096e2f9db7
Williano <paawilly17@gmail.com>
2021-03-17 18:49:53 -0500

    Update

--- dynamic_theming/lib/homepage_view.dart
+++ dynamic_theming/lib/homepage_view.dart
@@ -4,6 +4,22 @@ import 'package:get/get.dart';
 class HomepageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: context.theme.backgroundColor);
+    return Container(
+      color: context.theme.backgroundColor,
+      child: RaisedButton(
+        color: context.theme.buttonColor,
+        child: Text(
+          "Change Theme",
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          if (Get.isDarkMode) {
+            Get.changeThemeMode(ThemeMode.light);
+          } else {
+            Get.changeThemeMode(ThemeMode.dark);
+          }
+        },
+      ),
+    );
   }
 }
