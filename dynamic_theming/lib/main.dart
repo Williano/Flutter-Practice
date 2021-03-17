1475a7fe7221348e4dc5d0f05b23b3cc1e2287fb
Williano <paawilly17@gmail.com>
2021-03-17 18:39:24 -0500

    Update

--- dynamic_theming/lib/main.dart
+++ dynamic_theming/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_theming/homepage_view.dart';
 import 'package:dynamic_theming/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,54 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: Themes.light,
       darkTheme: Themes.dark,
-      home: MyHomePage(title: 'Flutter Dynamic Theming with Getx'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: HomepageView(),
     );
   }
 }
