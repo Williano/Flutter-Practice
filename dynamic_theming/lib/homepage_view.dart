b8b023203cafd681ff61ab7db698a78b0ab94f9c
Williano <paawilly17@gmail.com>
2021-03-17 18:42:18 -0500

    Update

--- dynamic_theming/lib/homepage_view.dart
+++ dynamic_theming/lib/homepage_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomepageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(color: context.theme.backgroundColor);
   }
 }
