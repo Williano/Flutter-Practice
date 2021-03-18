1dd24283e0a7953ba73ea1a7387899af7f9332e2
Williano <paawilly17@gmail.com>
2021-03-17 19:20:05 -0500

    Update

--- dynamic_theming/lib/homepage_view.dart
+++ dynamic_theming/lib/homepage_view.dart
@@ -1,27 +1,20 @@
+import 'package:dynamic_theming/themeservice.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomepageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: context.theme.backgroundColor,
-      child: Container(
-        width: 20,
-        height: 20,
+    return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
+      body: Center(
         child: RaisedButton(
           color: context.theme.buttonColor,
           child: Text(
-            "Change Theme",
+            'Change Theme',
             style: TextStyle(color: Colors.white),
           ),
-          onPressed: () {
-            if (Get.isDarkMode) {
-              Get.changeThemeMode(ThemeMode.light);
-            } else {
-              Get.changeThemeMode(ThemeMode.dark);
-            }
-          },
+          onPressed: ThemeService().switchTheme,
         ),
       ),
     );
